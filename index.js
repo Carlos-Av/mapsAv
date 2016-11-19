12a955883d95d5fb6f14ed35e88bdecf6ca4cb23
Carlos-Av <carlos17-av@hotmail.com>
2016-11-19 13:59:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-var http = require('http').server(app);
+var http = require('http').Server(app);
 //usando socketio
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));//Aqui se serviran archivos estaticos
-
+ 
 
 var port = process.env.PORT || 3000; //Esto lo exige heroku
 
